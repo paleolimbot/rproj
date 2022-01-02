c4d3f68859895c720db9aae3a6855061a2546121
Dewey Dunnington <dewey@fishandwhistle.net>
2022-01-01 21:40:11 -0400

    less brittle proj version test

--- tests/testthat/test-version.R
+++ tests/testthat/test-version.R
@@ -1,5 +1,5 @@
 
 test_that("proj_version() works", {
-  expect_identical(proj_version(runtime = TRUE), package_version("8.1.0"))
-  expect_identical(proj_version(runtime = FALSE), package_version("8.1.0"))
+  expect_s3_class(proj_version(runtime = TRUE), "package_version")
+  expect_s3_class(proj_version(runtime = FALSE), "package_version")
 })
